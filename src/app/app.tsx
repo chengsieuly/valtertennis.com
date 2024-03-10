477d02a027b928d629851f2e660b93587a6b373b
Cheng Ly <chengsieuly@gmail.com>
2024-03-10 07:49:24 -0700

    Setup nx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
-    <div>
-      <NxWelcome title="valtertennis.com" />
-    </div>
+    <h1 className="font-black">Hello world!!</h1>
   );
 }
 
